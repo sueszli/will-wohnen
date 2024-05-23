49cfbe86220ec387153faf20aea8ac2feef2b519
sueszli <jabaryyahya@gmail.com>
2024-05-23 23:43:42 +0200

    up

--- src/scrape_links.py
+++ src/scrape_links.py
@@ -6,12 +6,17 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from tqdm.asyncio import tqdm
+from pathlib import Path
 
 import random
 import asyncio
 import aiohttp
 import backoff
 from backoff import on_exception, expo
+
+
+filename = "links" + time.strftime("%Y-%m-%d_%H-%M-%S") + ".csv"
+output_path = Path.cwd() / "output" / filename
 
 
 def get_init_url() -> str:
@@ -31,7 +36,7 @@ def get_total_count(url: str) -> int:
     return int(elem.text.split()[0].replace(".", ""))
 
 
-def parse_links(html: str) -> List[str]:
+def parse_page(html: str) -> List[str]:
     soup = BeautifulSoup(html, "html.parser")
 
     elems = soup.find_all("div", {"class": "fvLiku"})
@@ -43,46 +48,20 @@ def parse_links(html: str) -> List[str]:
     return hrefs
 
 
-def dump_links(links: set[str]) -> None:
-    parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    data_dir = os.path.join(parent_dir, "data")
-    if not os.path.exists(data_dir):
-        os.makedirs(data_dir)
-
-    curr_time = time.strftime("%Y-%m-%d_%H-%M-%S")
-    path = os.path.join(data_dir, f"links_{curr_time}.csv")
-
-    with open(path, "w") as f:
-        f.write("\n".join(links))
-
-
-# @on_exception(expo, (aiohttp.ClientError, AssertionError), max_tries=3)  # retry on exceptions
+@on_exception(expo, (aiohttp.ClientError, AssertionError), max_tries=3)
 async def fetch_async(url: str) -> str:
-    global counter
+    await asyncio.sleep(random.uniform(0.125, 1))
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-        "Accept-Language": "en-US,en;q=0.5",
-    }
-    get_throttle = lambda: random.uniform(0.125, 1)
-    await asyncio.sleep(get_throttle())
+    async with aiohttp.ClientSession(raise_for_status=True) as session:
 
-    return "yo"
+        await asyncio.sleep(random.uniform(0.125, 1))
 
-    # await asyncio.sleep(get_throttle())
+        async with session.get(url) as response:
 
-    # async with aiohttp.ClientSession(raise_for_status=True) as session:
+            await asyncio.sleep(random.uniform(0.125, 1))
 
-    #     await asyncio.sleep(get_throttle())
-
-    #     async with session.get(url, headers=headers) as response:
-    #         assert response.status == 200, f"status code: {response.status}"
-
-    #         await asyncio.sleep(get_throttle())
-
-    #         counter += 1
-    #         print(f"progress: {counter}/{total_count}", end="\r")
-    #         return await response.text()
+            assert response.status == 200, f"status code: {response.status}"
+            return await response.text()
 
 
 async def main():
@@ -97,11 +76,8 @@ async def main():
     # print("parsing all links...")
     # links = set()
     # for result in results:
-    #     new_links = parse_links(result)
+    #     new_links = parse_page(result)
     #     links.update(new_links)
-
-    # print(f"dumping {len(links)}/{total_count} links to file...")
-    # dump_links(links)
 
 
 if __name__ == "__main__":
