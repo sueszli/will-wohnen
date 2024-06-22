d60aa3320258bba7f5785ac3ba4eaa987ca34e32
yahya jabary <61852663+sueszli@users.noreply.github.com>
2024-06-22 14:27:03 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,9 @@
-if ! command -v python3 &> /dev/null; then echo "python3 missing"; exit 1; fi
-if ! command -v pip &> /dev/null; then echo "pip missing"; exit 1; fi
-
 python3 -m pip install --upgrade pip
 
 # find out dependencies
-# rm -rf requirements.txt
-# pip install pipreqs
-# pipreqs .
+rm -rf requirements.txt
+pip install pipreqs
+pipreqs .
 
 # install dependencies
 pip install black
