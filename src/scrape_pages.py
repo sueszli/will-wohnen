dde4b5154e0b9018274e1c0724a928c523e94224
sueszli <jabaryyahya@gmail.com>
2024-08-26 14:08:10 +0200

    up

--- src/scrape_pages.py
+++ src/scrape_pages.py
@@ -1,33 +1,15 @@
-"""
-too fast, will get you rate limited
-"""
-
 import asyncio
 import csv
 import glob
 import json
 import random
+import time
 from pathlib import Path
-from typing import List
 
 import aiohttp
 from backoff import expo, on_exception
 from bs4 import BeautifulSoup
 from tqdm.asyncio import tqdm
-
-
-def load_links() -> List[str]:
-    datapath = glob.glob(str(Path("./data/*.csv")))
-    datapath = list(filter(lambda p: Path(p).name.startswith("links_") and Path(p).name.endswith(".csv"), datapath))
-    assert len(datapath) > 0
-    datapath.sort()
-    datapath = datapath[-1]
-
-    csvfile = csv.reader(open(datapath, "r"))
-    header = next(csvfile)
-    body = list(csvfile)
-    links = list(map(lambda row: row[0], body))
-    return links
 
 
 @on_exception(expo, (aiohttp.ClientError, AssertionError), max_tries=3)  # retry on exceptions
@@ -46,109 +28,65 @@ async def fetch_async(url: str) -> str:
 
 def parse_page(url: str, html: str) -> dict:
     soup = BeautifulSoup(html, "html.parser")
-    data = {}
+    data = {"url": url}
 
-    # url
-
-    data["url"] = url
+    def safe_extract(elem):
+        return elem.text.strip() if elem else None
 
     # title
-
-    elem = soup.find("h1", {"data-testid": "ad-detail-header"})
-    data["title"] = elem.text if elem else None
+    data["title"] = safe_extract(soup.find("h1", {"data-testid": "ad-detail-header"}))
 
     # last update section
-
-    elem = soup.find("span", {"data-testid": "ad-detail-ad-edit-date-top"})
-    data["last_update"] = elem.text if elem else None
+    data["last_update"] = safe_extract(soup.find("span", {"data-testid": "ad-detail-ad-edit-date-top"}))
 
     # price info section
-
-    price_info = {}
-
-    elem = soup.find("div", {"data-testid": "price-information-box"})
-    assert elem
-    elem = elem.find("div")
-    assert elem
-    elems = elem.find_all("div")  # type: ignore
-    assert len(elems) > 0
-    for elem in elems:
-        spans = elem.find_all("span")
-        fst = spans[0].text
-        snd = spans[1].text
-        price_info[fst] = snd
-
-    data["price_info"] = price_info
+    price_info = soup.find("div", {"data-testid": "price-information-box"})
+    if price_info:
+        price_details = price_info.find("div")
+        if price_details:
+            for elem in price_details.find_all("div"):
+                spans = elem.find_all("span")
+                if len(spans) == 2:
+                    data[spans[0].text.strip()] = spans[1].text.strip()
 
     # address section
+    data["address"] = safe_extract(soup.find("div", {"data-testid": "object-location-address"}))
 
-    elem = soup.find("div", {"data-testid": "object-location-address"})
-    data["address"] = elem.text if elem else None
-
-    # attributes section
-
-    attrs = {}
-
-    elems = soup.find_all("li", {"data-testid": "attribute-item"})
-    for elem in elems:
-        key_elem = elem.find("div", {"data-testid": "attribute-title"})
-        key = key_elem.text if key_elem else None
-        value_elem = elem.find("div", {"data-testid": "attribute-value"})
-        value = value_elem.text if value_elem else None
+    # Attributes section
+    for elem in soup.find_all("li", {"data-testid": "attribute-item"}):
+        key = safe_extract(elem.find("div", {"data-testid": "attribute-title"}))
+        value = safe_extract(elem.find("div", {"data-testid": "attribute-value"}))
         if key and value:
-            attrs[key] = value
-
-    data["attributes"] = attrs
+            data[key] = value
 
     # energy certificate section
-
-    elem = soup.find("div", {"data-testid": "energy-pass-box"})
-    data["energy_certificate"] = elem.text if elem else None
+    data["energy_certificate"] = safe_extract(soup.find("div", {"data-testid": "energy-pass-box"}))
 
     # description sections
-
-    desc = {}
-
-    elem = soup.find("div", {"data-testid": "ad-description-Objektbeschreibung"})
-    desc["description_general"] = elem.text if elem else None
-
-    elem = soup.find("div", {"data-testid": "ad-description-Lage"})
-    desc["description_location"] = elem.text if elem else None
-
-    elem = soup.find("div", {"data-testid": "ad-description-Ausstattung"})
-    desc["description_equipment"] = elem.text if elem else None
-
-    elem = soup.find("div", {"data-testid": "ad-description-Zusatzinformationen"})
-    desc["description_additional"] = elem.text if elem else None
-
-    elem = soup.find("div", {"data-testid": "ad-description-Preis und Detailinformation"})
-    desc["description_price"] = elem.text if elem else None
-
-    data["descriptions"] = desc
+    description_sections = [("description_general", "ad-description-Objektbeschreibung"), ("description_location", "ad-description-Lage"), ("description_equipment", "ad-description-Ausstattung"), ("description_additional", "ad-description-Zusatzinformationen"), ("description_price", "ad-description-Preis und Detailinformation")]
+    for key, testid in description_sections:
+        data[key] = safe_extract(soup.find("div", {"data-testid": testid}))
 
     return data
 
 
 async def main():
-    links = load_links()
-    print(f"total links: {len(links)}")
+    outputpath = Path.cwd() / "data" / ("pages_" + time.strftime("%Y-%m-%d_%H-%M-%S") + ".csv")
 
-    print("running async fetches...")
-    for url in tqdm(links):
+    inputpath = glob.glob(str(Path("./data/*.csv")))
+    inputpath = list(filter(lambda p: Path(p).name.startswith("links_") and Path(p).name.endswith(".csv"), inputpath))
+    assert len(inputpath) > 0
+    inputpath.sort()
+    inputpath = inputpath[-1]
+
+    inputfile = csv.reader(open(inputpath, "r"))
+    header = next(inputfile)
+
+    for row in tqdm(inputfile):
+        url = row[0]
         html = await fetch_async(url)
         data = parse_page(url, html)
         print(json.dumps(data, indent=4, ensure_ascii=False))
-
-    # tasks = [fetch_async(url) for url in links]
-    # results = await tqdm.gather(*tasks)
-
-    # print("parsing all pages...")
-    # pages: List[dict] = [parse_page(url, html) for url, html in zip(links, results)]
-    # flat_pages: dict = dict(enumerate(pages))
-
-    # print("dumping all pages...")
-    # # print(json.dumps(flat_pages, indent=4, ensure_ascii=False))
-    # dump_pages(flat_pages)
 
 
 if __name__ == "__main__":
