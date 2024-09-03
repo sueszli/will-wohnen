44c29c3aa2a8379b84686e2511528b5c7ef07c4a
sueszli <jabaryyahya@gmail.com>
2024-09-03 07:12:20 +0200

    up

--- src/4-graphdb_init.py
+++ src/4-graphdb_init.py
@@ -10,133 +10,6 @@ from tqdm import tqdm
 utils
 """
 
-
-def show_edges(tx):
-    result = tx.run("MATCH ()-[r]->() RETURN r")
-    for record in result:
-        print(record)
-
-
-def get_property(tx, url: str) -> dict:
-    result = tx.run("MATCH (p:Property {url: $url}) RETURN p", url=url)
-    return result.single().value()
-
-
-def get_company(tx, company_reference_id: str) -> dict:
-    result = tx.run("MATCH (c:Company {company_reference_id: $company_reference_id}) RETURN c", company_reference_id=company_reference_id)
-    return result.single().value()
-
-
-def get_broker(tx, company_broker_name: str) -> dict:
-    result = tx.run("MATCH (b:Broker {company_broker_name: $company_broker_name}) RETURN b", company_broker_name=company_broker_name)
-    return result.single().value()
-
-
-def get_employed_by(tx, company_broker_name: str, company_url: str) -> dict:
-    result = tx.run("MATCH (b:Broker {company_broker_name: $company_broker_name})-[r:employed_by]->(c:Company {company_url: $company_url}) RETURN r", company_broker_name=company_broker_name, company_url=company_url)
-    record = result.single()
-    return record.value() if record else None
-
-
-def get_manages(tx, company_broker_name: str, url: str) -> dict:
-    result = tx.run(
-        """
-        MATCH (b:Broker {company_broker_name: $company_broker_name})-[r:manages]->(p:Property {url: $url})
-        RETURN r {
-            .last_update,
-            maklerprovision: CASE WHEN r.maklerprovision = 'N/A' THEN null ELSE r.maklerprovision END
-        }
-        """,
-        company_broker_name=company_broker_name,
-        url=url,
-    )
-    return result.single().value()
-
-
-"""
-database initialization
-"""
-
-
-def show_nodes(tx):
-    result = tx.run("MATCH (n) RETURN n")
-    for record in result:
-        print(record)
-
-
-def store_nodes(tx, elem):
-    tx.run(
-        """
-        MERGE (p:Property {url: $url})
-        ON CREATE SET p.links_price = $links_price, p.description_price = $description_price, p.links_address = $links_address, p.bautyp = $bautyp, p.boeden = $boeden, p.energy_certificate = $energy_certificate, p.heizung = $heizung, p.links_type = $links_type, p.objekttyp = $objekttyp, p.stockwerke = $stockwerke, p.verfuegbar = $verfuegbar, p.wohnflaeche = $wohnflaeche, p.zimmer = $zimmer, p.zustand = $zustand
-        """,
-        elem,
-    )
-    tx.run(
-        """
-        MERGE (c:Company {company_reference_id: $company_reference_id})
-        ON CREATE SET c.company_url = $company_url, c.company_address = $company_address, c.company_name = $company_name
-        """,
-        elem,
-    )
-    tx.run(
-        """
-        MERGE (b:Broker {company_broker_name: $company_broker_name})
-        ON CREATE SET b.company_broker_name = $company_broker_name
-        """,
-        elem,
-    )
-
-
-def store_edges(tx, elem):
-    # broker -- employed_by --> company
-    tx.run(
-        """
-        MATCH (b:Broker {company_broker_name: $company_broker_name})
-        MATCH (c:Company {company_url: $company_url})
-        MERGE (b)-[:employed_by]->(c)
-        """,
-        elem,
-    )
-    # broker -- manages (using agreement) --> property
-    tx.run(
-        """
-        MATCH (b:Broker {company_broker_name: $company_broker_name})
-        MATCH (p:Property {url: $url})
-        MERGE (b)-[r:manages]->(p)
-        ON CREATE SET r.last_update = $last_update, r.maklerprovision = CASE WHEN $maklerprovision IS NOT NULL THEN $maklerprovision ELSE 'N/A' END
-        """,
-        elem,
-    )
-
-
-def init_db(tx):
-    inputpath = glob(str(Path.cwd() / "data" / "pages_*.csv"))
-    inputpath = list(filter(lambda p: Path(p).name.startswith("pages_") and Path(p).name.endswith(".csv"), inputpath))
-    assert len(inputpath) > 0
-    inputpath.sort()
-    inputpath = inputpath[-1]
-    inputfile = list(csv.reader(open(inputpath, "r")))
-    header = [word for word in inputfile[0]]
-    body = inputfile[1:]
-    dicts = list(map(lambda row: dict(zip(header, row)), body))  # convert to dict
-    dicts = list(map(lambda elem: {k: (v if v != "" else None) for k, v in elem.items()}, dicts))  # get null
-
-    for elem in tqdm(dicts):
-        tx.execute_write(store_nodes, elem)
-        # print(get_property(tx, elem["url"]))
-        # print(get_company(tx, elem["company_reference_id"]))
-        # print(get_broker(tx, elem["company_broker_name"]))
-
-        tx.execute_write(store_edges, elem)
-        # print(get_employed_by(tx, elem["company_broker_name"], elem["company_url"]))
-        # print(get_manages(tx, elem["company_broker_name"], elem["url"]))
-
-
-"""
-inference
-"""
-
 # {
 #     "agreement_commission_fee": "101370.0",
 #     "agreement_last_updated": "23.08.2024 06:10",
@@ -173,6 +46,73 @@ inference
 #     "property_usable_area": "341.96",
 #     "property_utilities": "1278.8"
 # }
+
+
+def init_db(tx):
+    inputpath = glob(str(Path.cwd() / "data" / "pages_*.csv"))
+    inputpath = list(filter(lambda p: Path(p).name.startswith("pages_") and Path(p).name.endswith(".csv"), inputpath))
+    assert len(inputpath) > 0
+    inputpath.sort()
+    inputpath = inputpath[-1]
+    inputfile = list(csv.reader(open(inputpath, "r")))
+    header = [word for word in inputfile[0]]
+    body = inputfile[1:]
+    dicts = list(map(lambda row: dict(zip(header, row)), body))  # convert to dict
+    dicts = list(map(lambda elem: {k: (v if v != "" else None) for k, v in elem.items()}, dicts))  # get null
+
+    for elem in tqdm(dicts):
+
+        def store_elem(tx, elem):
+            # broker
+            tx.run(
+                """
+                MERGE (b:Broker {broker_id: $broker_id})
+                """,
+                elem,
+            )
+            # company
+            tx.run(
+                """
+                MERGE (c:Company {company_id: $company_id})
+                ON CREATE SET c.company_address = $company_address, c.company_name = $company_name, c.company_url = $company_url
+                """,
+                elem,
+            )
+            # property
+            tx.run(
+                """
+                MERGE (p:Property {property_id: $property_id})
+                ON CREATE SET p.property_availabilty = $property_availabilty, p.property_balcony = $property_balcony, p.property_building_type = $property_building_type, p.property_completion = $property_completion, p.property_condition = $property_condition, p.property_district = $property_district, p.property_energy_certificate = $property_energy_certificate, p.property_features = $property_features, p.property_floor = $property_floor, p.property_flooring = $property_flooring, p.property_garden = $property_garden, p.property_heating = $property_heating, p.property_living_area = $property_living_area, p.property_loggia = $property_loggia, p.property_monthly_costs = $property_monthly_costs, p.property_other_costs = $property_other_costs, p.property_price = $property_price, p.property_rooms = $property_rooms, p.property_status = $property_status, p.property_terrace = $property_terrace, p.property_top_number = $property_top_number, p.property_total_area = $property_total_area, p.property_type = $property_type, p.property_units = $property_units, p.property_usable_area = $property_usable_area, p.property_utilities = $property_utilities
+                """,
+                elem,
+            )
+            # broker -- employed_by --> company
+            tx.run(
+                """
+                MATCH (b:Broker {broker_id: $broker_id})
+                MATCH (c:Company {company_id: $company_id})
+                MERGE (b)-[:employed_by]->(c)
+                """,
+                elem,
+            )
+            # broker -- manages (using agreement) --> property
+            tx.run(
+                """
+                MATCH (b:Broker {broker_id: $broker_id})
+                MATCH (p:Property {property_id: $property_id})
+                MERGE (b)-[r:manages]->(p)
+                ON CREATE SET r += {agreement_commission_fee: CASE WHEN $agreement_commission_fee IS NOT NULL THEN $agreement_commission_fee END,
+                                    agreement_last_updated: CASE WHEN $agreement_last_updated IS NOT NULL THEN $agreement_last_updated END}
+                """,
+                elem,
+            )
+
+        tx.execute_write(store_elem, elem)
+
+
+"""
+inference
+"""
 
 uri = "bolt://main:7687"
 auth = ("neo4j", "password")
